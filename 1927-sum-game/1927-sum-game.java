0b3624cf245a3c53a7eeb0709d3fcb61a8150920
Anshika Agarwal <anshikaagarwal795@gmail.com>
2026-08-24 02:53:52 +0530

    Time: 8 ms (53.24%), Space: 47.1 MB (26.62%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean sumGame(String num) {
+
+        int n = num.length();
+        int half = n / 2;
+
+        int leftSum = 0;
+        int rightSum = 0;
+        int leftQ = 0;
+        int rightQ = 0;
+
+        // Count sums and '?' in both halves
+        for (int i = 0; i < half; i++) {
+            if (num.charAt(i) == '?')
+                leftQ++;
+            else
+                leftSum += num.charAt(i) - '0';
+        }
+
+        for (int i = half; i < n; i++) {
+            if (num.charAt(i) == '?')
+                rightQ++;
+            else
+                rightSum += num.charAt(i) - '0';
+        }
+
+        // If number of ? is odd, Alice always wins
+        if ((leftQ + rightQ) % 2 == 1)
+            return true;
+
+        // Difference between existing sums
+        int diff = leftSum - rightSum;
+
+        // Difference in number of ? between halves
+        int qDiff = leftQ - rightQ;
+
+        /*
+         * Each pair of '?' can contribute a difference of
+         * at most 9, so:
+         *
+         * diff must be exactly balanced by qDiff * 9/2
+         */
+        return diff != -(qDiff * 9) / 2;
+    }
+}
